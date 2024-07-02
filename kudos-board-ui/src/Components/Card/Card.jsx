4faa6ca0277b5ab0096f2f7f6fa67a086dfdbb23
kaileychavez165 <kachavez165@gmail.com>
2024-07-02 10:35:38 -0700

    Add ability to upvote card

--- kudos-board-ui/src/Components/Card/Card.jsx
+++ kudos-board-ui/src/Components/Card/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Card.css";
 import axios from "axios";
 
@@ -8,7 +8,7 @@ const Card = ({ card }) => {
 
   const handleUpvote = async () => {
     try {
-      await axios.put(`http://localhost3000/cards/${card_id}/upvote`, { upvotes: upvotes + 1 });
+      await axios.put(`http://localhost:3000/cards/${card_id}/upvote`, { upvotes: upvotes + 1 });
       setUpvotes(upvotes + 1);
     } catch (error) {
       console.error("Error upvoting card:", error);
