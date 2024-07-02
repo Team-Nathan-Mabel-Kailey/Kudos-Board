70c3e16f5ef89ff27e38f2822889fbab9268ba2c
Mabel I.M. <ma.b.el.202423@gmail.com>
2024-07-02 10:50:09 -0700

    Edited footer. Added css to the cards. Still working on the boardlist modal.

--- kudos-board-ui/src/Components/App/App.jsx
+++ kudos-board-ui/src/Components/App/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+// import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "../Home/Home"
-import BoardList from "../BoardList/boardList"
+import BoardList from "../BoardList/BoardList"
 
 // what is it: paths to either home page OR page to specific board + list of its cards
 const App = () => {
