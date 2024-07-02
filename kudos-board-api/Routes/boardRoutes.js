13486c1d87a1ff8ed113f8c81dc8bd0bf0cadfb8
Nathan Quiroa <nquiroa@nquiroa-ltmzqp6.internal.salesforce.com>
2024-07-02 15:20:53 -0700

    fixing get card in boards

--- kudos-board-api/Routes/boardRoutes.js
+++ kudos-board-api/Routes/boardRoutes.js
@@ -5,6 +5,7 @@ const {
     getBoardById,
     updateBoard,
     deleteBoard,
+    getCardsInBoard
 } = require('../Controllers/boardController');
 
 const router = express.Router();
@@ -14,5 +15,6 @@ router.get('/', getAllBoards);
 router.get('/:board_id', getBoardById);
 router.put('/:board_id', updateBoard);
 router.delete('/:board_id', deleteBoard);
+router.get('/:board_id/cards', getCardsInBoard);
 
 module.exports = router;
