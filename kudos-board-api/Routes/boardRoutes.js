317241015a03ef5b9bb8f03aa15b81bd379d7c99
Nathan Quiroa <nquiroa@nquiroa-ltmzqp6.internal.salesforce.com>
2024-07-02 15:15:58 -0700

    mistake for board route

--- kudos-board-api/Routes/boardRoutes.js
+++ kudos-board-api/Routes/boardRoutes.js
@@ -5,7 +5,6 @@ const {
     getBoardById,
     updateBoard,
     deleteBoard,
-    getCardsInBoard,
 } = require('../Controllers/boardController');
 
 const router = express.Router();
@@ -15,6 +14,5 @@ router.get('/', getAllBoards);
 router.get('/:board_id', getBoardById);
 router.put('/:board_id', updateBoard);
 router.delete('/:board_id', deleteBoard);
-router.get('/:board_id/cards', getCardsInBoard);
 
 module.exports = router;
